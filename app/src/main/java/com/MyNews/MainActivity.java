3051dca92b47601705d99c80f7fb2ce765737875
thomas <thomas.dutru@gmail.com>
2019-05-02 16:18:32 +0200

    [ADD] -- Toolbar action : search and more option --

--- app/src/main/java/com/MyNews/MainActivity.java
+++ app/src/main/java/com/MyNews/MainActivity.java
@@ -1,7 +1,11 @@
 package com.MyNews;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.configureToolbar();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //3 - Handle actions on menu items
+        switch (item.getItemId()) {
+            case R.id.menu_activity_main_params:
+                Toast.makeText(this, "Nothing here", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_activity_main_search:
+                Toast.makeText(this, "Nothing here", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu); // Add menu to the toolbar
+        return true;
+    }
+
+    private void configureToolbar() {
+        Toolbar toolbar = findViewById(R.id.activity_main_toolbar); // Get toolbar view inside activity layout
+        setSupportActionBar(toolbar); // Set the toolbar
     }
 }
