6e8c08d68853805b70f8c2cbe559e43fe1d873c3
thomas <thomas.dutru@gmail.com>
2019-06-04 17:54:52 +0200

    [ADD] -- WebViewActivity with corresponding layout and Manifest Internet permission --

---
+++ app/src/main/java/com/mynews/controller/activities/WebViewActivity.java
@@ -0,0 +1,15 @@
+package com.mynews.controller.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mynews.R;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+    }
+}
